c2df9bb9bfae318748b34ee6225c5d0e525dab93
AztecaVilla <kory001_001@hotmail.com>
2024-06-23 23:35:41 -0600

    Update AztecaVilla.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AztecaVilla.py
@@ -0,0 +1,35 @@
+#Hola este es mi primer comentario
+# https://www.python.org/
+
+"""
+Esto es un 
+comentario
+en varias líneas
+
+"""
+
+''' 
+Esto tambien
+es un comentario
+en varias 
+lineas
+
+'''
+#variable
+
+var = 1
+
+#constante 
+
+DIAS_SEMANA = 7
+
+#datos primitivos
+
+int = 1
+decimal = 3.14
+boolean0 = True
+Boleano2 = False
+mi_string = 'Hi'
+
+
+print('Hola Python!')
